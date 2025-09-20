bc51a5e57dcf863c01faae0d7aee878f02631322
qkitzero <qkitzero@gmail.com>
2025-09-21 01:30:04 +0900

    add: log usecase

---
+++ src/application/logUseCase.ts
@@ -0,0 +1,22 @@
+import { Log } from "../domain/log/log";
+import { LogRepository } from "../infrastructure/logRepository";
+import { v4 } from "uuid";
+import { LogId } from "../domain/log/logId";
+import { LogLevel } from "../domain/log/logLevel";
+import { LogMessage } from "../domain/log/logMessage";
+import { LogTimestamp } from "../domain/log/logTimestamp";
+
+export class LogUseCase {
+  constructor(private readonly logRepository: LogRepository) {}
+
+  async createLog(level: string, message: string): Promise<Log> {
+    const log = new Log(
+      new LogId(v4()),
+      new LogLevel(level),
+      new LogMessage(message),
+      new LogTimestamp(new Date())
+    );
+    await this.logRepository.create(log);
+    return log;
+  }
+}
