7c43bacf4e992177621b5a0c1f3e7e62ceca9007
qkitzero <qkitzero@gmail.com>
2025-09-21 01:31:20 +0900

    add: log controller

--- src/interface/logController.ts
+++ src/interface/logController.ts
@@ -1,7 +1,19 @@
 import { Request, Response } from "express";
+import { LogUseCase } from "../application/logUseCase";
 
 export class LogController {
-  async getLogs(req: Request, res: Response): Promise<void> {
-    res.status(200).send({ message: "get logs" });
+  constructor(private readonly logUseCase: LogUseCase) {}
+
+  async createLog(req: Request, res: Response) {
+    const { level, message }: { level: string; message: string } = req.body;
+
+    const log = await this.logUseCase.createLog(level, message);
+
+    res.status(200).json({
+      id: log.id.value,
+      level: log.level.value,
+      message: log.message.value,
+      timestamp: log.timestamp.value,
+    });
   }
 }
