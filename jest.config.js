1ff4dc90ad88f0e8e95a717fd9a845651f9979ea
qkitzero <qkitzero@gmail.com>
2025-09-21 22:36:13 +0900

    add: jest

---
+++ jest.config.js
@@ -0,0 +1,10 @@
+/** @type {import("jest").Config} **/
+module.exports = {
+  preset: "ts-jest",
+  testEnvironment: "node",
+  transform: {
+    "^.+\\.tsx?$": "ts-jest",
+    "^.+\\.jsx?$": "ts-jest",
+  },
+  transformIgnorePatterns: ["<rootDir>/node_modules/(?!uuid)"],
+};
