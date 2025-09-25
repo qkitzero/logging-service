be4bf6df12fde35b3c13f86c973b9ace30bec12b
qkitzero <qkitzero@gmail.com>
2025-09-25 21:33:03 +0900

    add: service name

--- src/domain/log/log.test.ts
+++ src/domain/log/log.test.ts
@@ -1,5 +1,6 @@
 import { Log } from "./log";
 import { LogId } from "./logId";
+import { ServiceName } from "./serviceName";
 import { LogLevel } from "./logLevel";
 import { LogMessage } from "./logMessage";
 import { LogTimestamp } from "./logTimestamp";
@@ -8,13 +9,15 @@ import { v4 } from "uuid";
 describe("Log", () => {
   it("should create a Log instance with correct values", () => {
     const id = new LogId(v4());
+    const serviceName = new ServiceName("test-service");
     const level = new LogLevel(LogLevel.INFO);
     const message = new LogMessage("Test log message");
     const timestamp = new LogTimestamp(new Date());
 
-    const log = new Log(id, level, message, timestamp);
+    const log = new Log(id, serviceName, level, message, timestamp);
 
     expect(log.id).toBe(id);
+    expect(log.serviceName).toBe(serviceName);
     expect(log.level).toBe(level);
     expect(log.message).toBe(message);
     expect(log.timestamp).toBe(timestamp);
--- src/domain/log/log.ts
+++ src/domain/log/log.ts
@@ -1,4 +1,5 @@
 import { LogId } from "./logId";
+import { ServiceName } from "./serviceName";
 import { LogLevel } from "./logLevel";
 import { LogMessage } from "./logMessage";
 import { LogTimestamp } from "./logTimestamp";
@@ -6,6 +7,7 @@ import { LogTimestamp } from "./logTimestamp";
 export class Log {
   constructor(
     readonly id: LogId,
+    readonly serviceName: ServiceName,
     readonly level: LogLevel,
     readonly message: LogMessage,
     readonly timestamp: LogTimestamp
