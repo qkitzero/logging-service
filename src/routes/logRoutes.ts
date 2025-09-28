58652b9d3a0dd4115e3f0beeae5af4747c01d96f
qkitzero <qkitzero@gmail.com>
2025-09-28 23:35:28 +0900

    update: repository

--- src/routes/logRoutes.ts
+++ src/routes/logRoutes.ts
@@ -2,10 +2,12 @@ import { Router } from "express";
 import { LogController } from "../interface/logController";
 import { LogRepository } from "../infrastructure/logRepository";
 import { LogUseCase } from "../application/logUseCase";
+import { PrismaClient } from "@prisma/client";
 
 const router = Router();
 
-const logRepository = new LogRepository();
+const prisma = new PrismaClient();
+const logRepository = new LogRepository(prisma);
 const logUseCase = new LogUseCase(logRepository);
 const logController = new LogController(logUseCase);
 
