3d7c83f37957955d85bd88bd1c0f584fac974e0d
qkitzero <qkitzero@gmail.com>
2025-10-03 19:33:03 +0900

    fix: lint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -30,6 +30,7 @@ export default tseslint.config(
       '@typescript-eslint/no-unused-vars': [
         'error',
         {
+          args: 'all',
           argsIgnorePattern: '^_',
           varsIgnorePattern: '^_',
           caughtErrorsIgnorePattern: '^_',
