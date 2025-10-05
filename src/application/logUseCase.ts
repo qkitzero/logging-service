b416c1b9183a1219f2f69d73fbfede73bc2f3b77
qkitzero <qkitzero@gmail.com>
2025-10-05 23:23:46 +0900

    fix: log repository interface

--- src/application/logUseCase.ts
+++ src/application/logUseCase.ts
@@ -4,11 +4,11 @@ import { Id as LogId } from '../domain/log/id';
 import { Level as LogLevel } from '../domain/log/level';
 import { Log } from '../domain/log/log';
 import { Message as LogMessage } from '../domain/log/message';
+import { ILogRepository } from '../domain/log/repository';
 import { Timestamp as LogTimestamp } from '../domain/log/timestamp';
-import { LogRepository } from '../infrastructure/logRepository';
 
 export class LogUseCase {
-  constructor(private readonly logRepository: LogRepository) {}
+  constructor(private readonly logRepository: ILogRepository) {}
 
   async createLog(serviceName: string, level: string, message: string): Promise<Log> {
     const log = new Log(
